87456b239a38ce1a02028cc4b4030019a3c0ccc1
FosterSOAsare <fostersoasare@gmail.com>
2023-10-02 19:11:22 +0000

    Project startup

---
+++ src/components/molecules/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+	return (
+		<header>
+			<div className="container header__container">
+				<div></div>
+
+				<nav></nav>
+				<div></div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/molecules/Header";
+// import Footer from "../components/molecules/Footer";
+
+const Homepage = () => {
+	return (
+		<div>
+			<Header />
+			{/* <Footer /> */}
+		</div>
+	);
+};
+
+export default Homepage;
